24315c937b8b966442b9dfeaa9c24f3fcf1b86f6
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2019-02-18 08:47:10 +0100

    Global dropdown element - custom values and attr change callback

---
+++ src/components/SchibstedDropdown.js
@@ -0,0 +1,63 @@
+export default class extends HTMLElement {
+  constructor(changeCallback, values = []) {
+    super();
+    this.changeCallback = changeCallback;
+    this.values = values;
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ['data-value'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.changeCallback && oldValue !== newValue) this.changeCallback(newValue);
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.className = 'schibsted-dropdown';
+    this.element.innerHTML = `
+        <div class="schibsted-dropdown-btn-group"></div>
+        <div class="schibsted-dropdown-options"></div>
+    `;
+    this.appendChild(this.element);
+    const btngroup = document.querySelector('.schibsted-dropdown-btn-group');
+    this.options = document.querySelector('.schibsted-dropdown-options');
+    this.options.onblur = () => {
+      this.element.classList.remove('open');
+    };
+    this.label = document.createElement('label');
+    this.label.className = 'schibsted-dropdown-label';
+    this.label.innerText = this.getAttribute('data-value').toUpperCase();
+
+    this.button = document.createElement('button');
+    this.button.className = 'schibsted-dropdown-btn';
+
+    btngroup.appendChild(this.label);
+    btngroup.appendChild(this.button);
+    this.button.onclick = () => {
+      if (this.element.classList.contains('open')) {
+        this.element.classList.remove('open');
+      } else {
+        document.querySelector('.schibsted-dropdown-options').focus();
+        this.element.classList.add('open');
+      }
+    };
+    this.renderOptions();
+  }
+
+  renderOptions() {
+    this.values.forEach((value) => {
+      const option = document.createElement('li');
+      option.dataset.val = value.val;
+      option.innerText = value.text;
+      this.options.appendChild(option);
+      option.onclick = () => {
+        this.label.innerText = value.text.toUpperCase();
+        this.element.classList.remove('open');
+        this.setAttribute('data-value', value.val);
+      };
+    });
+  }
+}
