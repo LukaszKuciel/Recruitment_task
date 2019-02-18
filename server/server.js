77216ad9757a1f0a6d71ac368f4a542bed8c2b22
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2019-02-18 08:43:49 +0100

    simple API - cors + node fs.readFileSync

--- server/server.js
+++ server/server.js
@@ -1,31 +1,54 @@
-'use strict';
+const fs = require('fs');
 
 const Hapi = require('hapi');
 
-const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
+const server = new Hapi.Server({
+  port: 3000,
+  host: 'localhost',
+  routes: { cors: true }
 });
 
 server.route({
-    method: 'GET',
-    path: '/advertisers/{datatype}',
-    handler: (request, h) => {
+  method: 'OPTIONS',
+  path: '/advertisers',
+  handler: (request, reply) => {
+    const response = reply.response({}).code(200);
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    response.header('Access-Control-Allow-Methods', 'GET, POST, HEAD, OPTIONS');
+    return response;
+  }
+});
 
-        return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
+server.route({
+  method: 'GET',
+  path: '/advertisers',
+  handler: (request, reply) => {
+    const { type } = request.query;
+    let response;
+    try {
+      response = reply.response(fs.readFileSync(`./server/data/advertisers/${type}/response.${type}`));
+      response.header('Content-Type', `application/${type}`);
+      response.header('Access-Control-Allow-Origin', '*');
+      response.header('Allow', 'GET, POST, HEAD, OPTIONS');
+      response.header('Vary', 'Accept');
+      response.type(`application/${type}`);
+      response.header('Access-Control-Expose-Headers', 'Vary, Allow, Content-Type');
+    } catch (error) {
+      console.log(error);
     }
+    return response;
+  }
 });
 
 const init = async () => {
-
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
